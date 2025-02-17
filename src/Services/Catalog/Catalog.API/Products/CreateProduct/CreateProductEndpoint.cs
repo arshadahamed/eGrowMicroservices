6522c2a15495154588376ed0a7980baad7ce51bc
arshadahamed <arshadpayoneer@gmail.com>
2025-02-17 21:30:12 +0530

    Developed POST Endpoint with Carter implements ICarterModule for Minimal Apis sand created Global using

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -3,6 +3,25 @@
 public record CreateProductRequest(string Name, List<string> Categories, string Description, string ImageFile, decimal Price);
 public record CreateProductResponse(Guid Id);
 
-public class CreateProductEndpoint
+public class CreateProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products",
+            async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<CreateProductResponse>();
+
+            return Results.Created($"/products/{response.Id}", response);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Creates a new product")
+        .WithDescription("Creates a new product in the catalog");
+    }
 }
