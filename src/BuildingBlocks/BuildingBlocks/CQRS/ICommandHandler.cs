c616a05be45b8ccccdc4be083c1b427e315baa9b
arshadahamed <arshadpayoneer@gmail.com>
2025-02-17 16:48:19 +0530

    Created Interfaces for Command Query using Meditar for generic CQRS

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+// This interface defines a command handler for commands that do not return a response (Unit represents void).
+public interface ICommandHandler<in TCommand>
+    : ICommandHandler<TCommand, Unit>
+    where TCommand : ICommand<Unit>
+{
+}
+
+// This interface defines a command handler that processes a command (TCommand) and returns a response (TResponse).
+public interface ICommandHandler<in TCommand, TResponse> 
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+{
+}
